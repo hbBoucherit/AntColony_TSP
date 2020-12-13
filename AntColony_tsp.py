6fe9ea05c8f1532c67e7f19f73fe36e8b63ee869
hbBoucherit <hboucheri001@ensc.fr>
2020-12-13 13:56:41 +0100

    Combined graphics + Julien's Algorithm

--- AntColony_tsp.py
+++ AntColony_tsp.py
@@ -1,184 +1,172 @@
-from copy import deepcopy
+import numpy as np
+import random
 import cv2
-import numpy as np
 
-_CITIES = [
-    ("Bordeaux", (44.833333, -0.566667)),
-    ("Paris", (48.8566969, 2.3514616)),
-    ("Nice", (43.7009358, 7.2683912)),
-    ("Lyon", (45.7578137, 4.8320114)),
-    ("Nantes", (47.2186371, -1.5541362)),
-    ("Brest", (48.4, -4.483333)),
-    ("Lille", (50.633333, 3.066667)),
-    ("Clermont-Ferrand", (45.783333, 3.083333)),
-    ("Strasbourg", (48.583333, 7.75)),
-    ("Poitiers", (46.583333, 0.333333)),
-    ("Angers", (47.466667, -0.55)),
-    ("Montpellier", (43.6, 3.883333)),
-    ("Caen", (49.183333, -0.35)),
-    ("Rennes", (48.083333, -1.683333)),
-    ("Pau", (43.3, -0.366667)),
-]
+# Liste des villes
+_CITIES = (("Bordeaux", (44.833333,-0.566667)), ("Paris",(48.8566969,2.3514616)),("Nice",(43.7009358,7.2683912)),
+               ("Lyon",(45.7578137,4.8320114)),("Nantes",(47.2186371,-1.5541362)),("Brest",(48.4,-4.483333)),("Lille",(50.633333,3.066667)),
+               ("Clermont-Ferrand",(45.783333,3.083333)),("Strasbourg",(48.583333,7.75)),("Poitiers",(46.583333,0.333333)),
+               ("Angers",(47.466667,-0.55)),("Montpellier",(43.6,3.883333)),("Caen",(49.183333,-0.35)),("Rennes",(48.083333,-1.683333)),("Pau",(43.3,-0.366667)))
 
-import numpy as np
-def distance(a, b):
-    (x1, y1), (x2, y2) = (a, b)
-    return np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+# Nombre de villes
+nb_cities = len(_CITIES)
 
-class ant:
-    def __init__(self, initial_city, alpha, beta, rho, const):
-        self.location = initial_city
-        self.start = initial_city
-        # liste des villes que la fourmi peut visiter, ce sont toutes les villes au tout début
-        self.possible_cities = deepcopy(_CITIES)
-        self.distance_traveled = 0
-        self.alpha = alpha
-        self.beta = beta
-        self.rho = rho # coef d'évaporation
-        self.const = const  # constante Q de dépôt de phéromones
-        self.visited_cities = []  # dans l'ordre de visite
-        self.tour_complete = False  # true quand le tour est terminé
+# Nombre de fourmis
+nb_fourmis = nb_cities
 
-    def run(self):
-        while self.possible_cities:
-            next_city = self.find_next_city(self.location)
-            self.traverse(self.location, next_city)
-        self.tour_complete = True
-        self.update_distance_traveled(self.location, self.start)
-        self.add_pheromones()
+nb_cycles = 100
 
-    # on calcule la somme de l'attractiveness de chaque ville qui peut être visitée par la fourmi
-    def sum_attractiveness_city(self, actual_city):
-        attractiveness = 0
-        for i in range(len(self.possible_cities)):
-            visibility = float(1/distance(actual_city[1], self.possible_cities[i][1]))
-            attractiveness += ((visibility**self.alpha) * (pheromone_map[_CITIES.index(actual_city)][_CITIES.index(self.possible_cities[i])]**self.beta))
-        return attractiveness
+# Liste permettant de placer au départ les fourmis
+_RANDOM = list(range(0, nb_fourmis))
 
-    def find_next_city(self, actual_city):
-        # on suppose que la ville suivante est la première de la liste
-        next_city = self.possible_cities[0]
-        visibility0 = float(1/distance(actual_city[1], next_city[1]))  # inverse de la distance
-        attractiveness0 = ((visibility0**self.alpha) * (pheromone_map[_CITIES.index(actual_city)][_CITIES.index(next_city)]**self.beta))
-        proba0 = attractiveness0 / self.sum_attractiveness_city(actual_city)
+_PHEROMONES = np.zeros(shape=(nb_cities, nb_cities))
+_DISTANCE_CITIES = np.zeros(shape=(nb_cities, nb_cities))
+_FOURMIS = []
+_DISTANCE_FOURMIS = []
+_PHEROMONES_ARCS = np.zeros(shape=(nb_cities, nb_cities))
 
-        # on parcourt la liste et vérifie si il y a une ville qui a une plus grande 'attractiveness' 
-        for i in range(len(self.possible_cities)):
-            visibility = float(1/distance(actual_city[1], self.possible_cities[i][1]))
-            attractiveness = ((visibility**self.alpha) * (pheromone_map[_CITIES.index(actual_city)][_CITIES.index(self.possible_cities[i])]**self.beta))
-            proba = attractiveness / self.sum_attractiveness_city(actual_city)
-            if (proba > proba0):
-                next_city = self.possible_cities[i]
-        return next_city
-
-    def traverse(self, start, end):
-        self.update_visited_cities(end)
-        self.update_distance_traveled(start, end)
-        self.location = end
-
-    def update_visited_cities(self, new_city):
-        if new_city not in self.visited_cities :
-            self.visited_cities.append(new_city)
-        if new_city in self.possible_cities:
-            self.possible_cities.remove(new_city)
-
-    def update_distance_traveled(self, start, end):
-        if self.tour_complete == True : 
-            self.distance_traveled += float(distance(start[1], self.start[1]))
-        else :
-            self.distance_traveled += float(distance(start[1], end[1]))
-
-    def get_visited_cities(self):
-        if self.tour_complete:
-            self.visited_cities.append(self.start)
-            return self.visited_cities
-        return None
-
-    def get_distance_traveled(self):
-            return self.distance_traveled
-
-    def add_pheromones(self):
-        for i in range (0,len(self.visited_cities)-1) :
-            # on récupère l'indice de deux villes successives 
-            index_city1 = _CITIES.index(self.visited_cities[i])
-            index_city2 = _CITIES.index(self.visited_cities[i+1])
+alpha = 1
+beta = 5
+Q = 100 # constante de dépôt de phéromones
+P = 0.5 # coefficient vitesse evaporation
+    
+def start():
+    for i in range(nb_fourmis):
+        # les fourmis sont placées aléatoirement sur une des nb_cities villes
+        random_item = random.choice(_RANDOM)
+        _FOURMIS.append([random_item])
+        _RANDOM.remove(random_item)
+    for i in range(nb_cities):
+        for j in range(nb_cities):
+            # les phéromones sont initialisés avec une petite constante positive
+            _PHEROMONES[i, j] = 0.1 
+            _DISTANCE_CITIES[i, j] = distance(_CITIES[i][1], _CITIES[j][1])
+    return _FOURMIS
         
-            # on prend on compte l'évaporation aussi 
-            pheromone_map[index_city1][index_city2] = (1-self.rho)*pheromone_map[index_city1][index_city2] + float(self.const/self.distance_traveled)
+def nextStep(_FOURMI):
+    _CITIES = []
+    _PROBS = []
+    for city in range(nb_cities):
+        if city not in _FOURMI:
+            visibility = 1/_DISTANCE_CITIES[_FOURMI[-1], city]
+            pheromone = _PHEROMONES[_FOURMI[-1], city]
+            prob = (visibility**beta)*(pheromone**alpha)
+            _CITIES.append(city)
+            _PROBS.append(prob) 
+    _PROBS = _PROBS/sum(_PROBS)
+    return np.random.choice(_CITIES, 1, p=_PROBS)[0]
 
-# on initialise matrice des pheromones avec des 0.1 partout
-pheromone_map = [[0.1] * len(_CITIES) for i in range(len(_CITIES))]
+def play():
+    start()
+    _BESTPATH = []
+    bestPath_distance = float('inf')
+    for i in range(nb_cycles):
+        for j in range(nb_cities):
+            for _FOURMI in _FOURMIS:
+                if(j == nb_cities-1):
+                    _FOURMI.append(_FOURMI[0])
+                else:
+                    _FOURMI.append(nextStep(_FOURMI))  
+        updatePheromone(_FOURMIS)
+        bestPath_distance, _BESTPATH = chooseBestPath(_FOURMIS, _DISTANCE_FOURMIS, bestPath_distance, _BESTPATH)
+        deleteMemory(_FOURMIS, _DISTANCE_FOURMIS, _PHEROMONES_ARCS)
+    
+    #liste de villes avec des coordonnées assez séparées pour pouvoir bien les représenter sur la map 
+    dict_cities = {
+        "Bordeaux" : [230, 460],
+        "Paris" : [370, 200],
+        "Nice" : [570, 550],
+        "Lyon" : [470, 410],
+        "Nantes" : [190, 300],
+        "Brest" : [70, 210],
+        "Lille" : [400, 110],
+        "Clermont-Ferrand" : [390, 380],
+        "Strasbourg" : [590, 250],
+        "Poitiers" : [300, 340],
+        "Angers":  [250, 290],
+        "Montpellier" : [420, 550],
+        "Caen" : [260, 190],
+        "Rennes" : [190, 240],
+        "Pau" : [230, 560],
+    }
 
-from random import randint
-colony = [] # on initialise une colonie de fourmis
-for i in range(1000) : 
-    # ville initiale choisie au hasard
-    city = _CITIES[randint(0,len(_CITIES)-1)]
-    colony.append(ant(city, 1, 1, 0.05, 100))
+    shortest_distance = bestPath_distance
+    shortest_route = []
+    for i in range(len(_BESTPATH)):
+        shortest_route.append(_CITIES[_BESTPATH[i]])
 
-# on suppose que la distance la plus courte est celle de la première fourmi 
-ant = 0 # indice de la fourmi ayant fait le chemin le plus court (sera utile pour afficher la distance cumulée)
-colony[0].update_visited_cities(colony[0].location)
-colony[0].run() 
-shortest_distance = colony[0].get_distance_traveled() #distance numérique
-shortest_route = colony[0].get_visited_cities() #chemin correspondant à la distance la plus courte 
+    #il faut modifier les coordonnées dans shortest_route pour pouvoir bien afficher chaque ville sur la map 
+    updated_shortest_route = [[0]*2 for i in range(len(shortest_route))]
+    for i in range(len(shortest_route)):
+        updated_shortest_route[i][0] = str(shortest_route[i][0])
+        updated_shortest_route[i][1] = dict_cities[str(shortest_route[i][0])]
+    
+    print('---------------------------')
+    print('Shortest_distance : ' + str(shortest_distance))
+    print('Shortest route : ' + str(updated_shortest_route))
 
-# les fourmis commencent leurs tours, on parcourt la liste et vérifie s'il existe une distance plus courte que celle qu'on a supposé
-for i in range(1,len(colony)) :
-    colony[i].update_visited_cities(colony[i].location)
-    colony[i].run() 
-    if colony[i].get_distance_traveled() < shortest_distance :
-        ant = i
-        shortest_distance = colony[i].get_distance_traveled()
-        shortest_route = colony[i].get_visited_cities()
+    #image de la carte de france
 
-# on affiche la distance la plus courte ainsi que le chemin correspondant
-print("Shortest distance : " + str(shortest_distance))
+    france_map = cv2.imread('france_map.png')
+    cumulative_distance = 0 
+    for i in range(len(updated_shortest_route)) :
+        # on affiche le titre + les villes avec les fonctions d'OpenCV putText et circle
+        cv2.putText(france_map, "Shortest distance traveled : " + str(round(shortest_distance,2)), (180, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,0), 1)
+        cv2.putText(france_map, updated_shortest_route[i][0], (int(updated_shortest_route[i][1][0]-10),int(updated_shortest_route[i][1][1])-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1)
+        cv2.circle(france_map,(int(updated_shortest_route[i][1][0]),int(updated_shortest_route[i][1][1])), 3, (0,0,255), -1)
+    for i in range(len(updated_shortest_route)-1) :
+        # on affiche la distance cumulée
+        cumulative_distance += float(distance(shortest_route[i][1], shortest_route[i+1][1]))
+        cv2.putText(france_map, "Cumulative distance : " + str(round(cumulative_distance,2)), (260, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1)
+        # on trace le chemin pris 
+        cv2.arrowedLine(france_map, (int(updated_shortest_route[i][1][0]),int(updated_shortest_route[i][1][1])), (int(updated_shortest_route[i+1][1][0]),int(updated_shortest_route[i+1][1][1])), (0,180,0), 1)
+        cv2.imshow("Ant colony best path", france_map)
+        # tant que le tour n'est pas fini, on attend 0.5s avant de tracer le chemin vers la ville suivante choisie 
+        cv2.waitKey(500)
+        cv2.putText(france_map, "Cumulative distance : " + str(round(cumulative_distance,2)), (260, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
+    # si le tour est fini, on laisse la fenêtre ouverte
+    cv2.waitKey(0)
 
-#liste de villes avec des coordonnées assez séparées pour pouvoir bien les représenter sur la map 
-dict_cities = {
-    "Bordeaux" : [230, 460],
-    "Paris" : [370, 200],
-    "Nice" : [570, 550],
-    "Lyon" : [470, 410],
-    "Nantes" : [190, 300],
-    "Brest" : [70, 210],
-    "Lille" : [400, 110],
-    "Clermont-Ferrand" : [390, 380],
-    "Strasbourg" : [590, 250],
-    "Poitiers" : [300, 340],
-    "Angers":  [250, 290],
-    "Montpellier" : [420, 550],
-    "Caen" : [260, 190],
-    "Rennes" : [190, 240],
-    "Pau" : [230, 560],
-}
+    
+def calcDistances(_FOURMIS):
+    #calcule la distance totale parcourue par chaque fourmi
+    for _FOURMI in _FOURMIS:
+        distance = 0
+        for city in range(nb_cities):
+            distance = distance + _DISTANCE_CITIES[_FOURMI[city], _FOURMI[city+1]]
+        _DISTANCE_FOURMIS.append(distance)
+    
+def calcPheromoneArcs(_FOURMIS):
+    for i in range(nb_fourmis):
+        for city in range(nb_cities):
+            _PHEROMONES_ARCS[_FOURMIS[i][city], _FOURMIS[i][city+1]] = _PHEROMONES_ARCS[_FOURMIS[i][city], _FOURMIS[i][city+1]] + Q/_DISTANCE_FOURMIS[i] 
+            
+def updatePheromone(_FOURMIS):
+    calcDistances(_FOURMIS)
+    calcPheromoneArcs(_FOURMIS)
+    for i in range(nb_cities):
+        for j in range(nb_cities):
+            _PHEROMONES[i,j] = P*_PHEROMONES[i,j] + _PHEROMONES_ARCS[i,j]
 
-#il faut modifier les coordonnées dans shortest_route pour pouvoir bien afficher chaque ville sur la map 
-updated_shortest_route = [[0]*2 for i in range(len(shortest_route))]
-for i in range(len(shortest_route)):
-    updated_shortest_route[i][0] = str(shortest_route[i][0])
-    updated_shortest_route[i][1] = dict_cities[str(shortest_route[i][0])]
-print('Shortest route : ' + str(updated_shortest_route))
-#print(pheromone_map)
+def chooseBestPath(_FOURMIS, _DISTANCE_FOURMIS, bestPath_distance, _BESTPATH):
+    minDistance = min(_DISTANCE_FOURMIS)
+    minDistance_index = _DISTANCE_FOURMIS.index(minDistance)
+    minDistance_path = np.copy(_FOURMIS[minDistance_index])
+    if(minDistance < bestPath_distance):
+        return minDistance, minDistance_path
+    else:
+        return bestPath_distance, _BESTPATH
+    
+def deleteMemory(_FOURMIS, _DISTANCE_FOURMIS, _PHEROMONES_ARCS):
+    for _FOURMI in _FOURMIS:
+        while(len(_FOURMI)>1):
+            _FOURMI.pop()  
+    _DISTANCE_FOURMIS[:] = []
+    _PHEROMONES_ARCS = np.zeros(shape=(nb_cities, nb_cities))
+   
+def distance(a,b):
+    (x1,y1),(x2,y2) = (a,b)
+    return np.sqrt((x1-x2)**2+(y1-y2)**2) 
 
-#image de la carte de france
-france_map = cv2.imread('france_map.png')
-cumulative_distance = 0 
-for i in range(len(updated_shortest_route)) :
-    # on affiche le titre + les villes avec les fonctions d'OpenCV putText et circle
-    cv2.putText(france_map, "Shortest distance traveled : " + str(round(shortest_distance,2)), (180, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,0), 1)
-    cv2.putText(france_map, updated_shortest_route[i][0], (int(updated_shortest_route[i][1][0]-10),int(updated_shortest_route[i][1][1])-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1)
-    cv2.circle(france_map,(int(updated_shortest_route[i][1][0]),int(updated_shortest_route[i][1][1])), 3, (0,0,255), -1)
-for i in range(len(updated_shortest_route)-1) :
-    # on affiche la distance cumulée
-    cumulative_distance += float(distance(colony[ant].visited_cities[i][1], colony[ant].visited_cities[i+1][1]))
-    cv2.putText(france_map, "Cumulative distance : " + str(round(cumulative_distance,2)), (260, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1)
-    # on trace le chemin pris 
-    cv2.arrowedLine(france_map, (int(updated_shortest_route[i][1][0]),int(updated_shortest_route[i][1][1])), (int(updated_shortest_route[i+1][1][0]),int(updated_shortest_route[i+1][1][1])), (0,180,0), 1)
-    cv2.imshow("Ant colony best path", france_map)
-    # tant que le tour n'est pas fini, on attend 0.3s avant de tracer le chemin vers la ville suivante choisie 
-    cv2.waitKey(500)
-    cv2.putText(france_map, "Cumulative distance : " + str(round(cumulative_distance,2)), (260, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
-# si le tour est fini, on laisse la fenêtre ouverte
-cv2.waitKey(0)
+play()
+
